99404a2b9282d3fb572839e1cacda915683b8e1a
Misha <term11term@gmail.com>
2022-10-16 01:14:40 +0300

    Config and views

---
+++ src/main/java/com/nikitin/codeinsideproject/CodeInsideProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nikitin.codeinsideproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeInsideProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeInsideProjectApplication.class, args);
+    }
+
+}
