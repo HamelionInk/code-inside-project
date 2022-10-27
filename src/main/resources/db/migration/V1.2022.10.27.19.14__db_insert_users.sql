3d1601462468bcc3c8ea4115ab670c7854492af7
Misha <term11term@gmail.com>
2022-10-28 00:09:34 +0300

    Refactor and add Docker

--- src/main/resources/db/migration/V1.2022.10.27.19.14__db_insert_users.sql
+++ src/main/resources/db/migration/V1.2022.10.27.19.14__db_insert_users.sql
@@ -2,5 +2,5 @@ insert into person (person_id, age, email, password, role, username)
 VALUES (gen_random_uuid(), 23, 'user@gmail.com', 'user', 'USER', 'user');
 
 insert into person (person_id, age, email, password, role, username)
-VALUES (gen_random_uuid(), 43, 'admin@gmail.com', 'admin', 'ADMIN', 'user');
+VALUES (gen_random_uuid(), 43, 'admin@gmail.com', 'admin', 'ADMIN', 'admin');
 
